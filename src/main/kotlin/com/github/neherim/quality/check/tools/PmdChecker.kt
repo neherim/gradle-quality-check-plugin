4eb5889965c8693e573da40fb2e79d026b864eb2
Mikhailovskii <mikhailovskii@nsd.ru>
2019-10-29 17:46:08 +0300

    default pmd/checkstyle ruleset

--- src/main/kotlin/com/github/neherim/quality/check/tools/PmdChecker.kt
+++ src/main/kotlin/com/github/neherim/quality/check/tools/PmdChecker.kt
@@ -4,25 +4,36 @@ import org.gradle.api.Project
 import org.gradle.api.plugins.quality.Pmd
 import org.gradle.api.plugins.quality.PmdExtension
 import org.gradle.api.plugins.quality.PmdPlugin
+import java.io.File
 
 object PmdChecker {
     fun addPlugin(root: Project, target: Project, ext: PmdQualityExtension) {
         if (ext.enabled) {
             target.plugins.apply(PmdPlugin::class.java)
+            val ruleSetFile = root.file(ext.ruleSetFile)
+            if (!ruleSetFile.exists()) {
+                createDefaultRuleSet(ruleSetFile)
+            }
             target.extensions.configure(PmdExtension::class.java) {
                 it.toolVersion = ext.toolVersion
                 it.isIgnoreFailures = ext.ignoreFailures
                 it.isConsoleOutput = ext.consoleOutput
+                it.ruleSetFiles = root.files(ruleSetFile)
 
-                if (root.file(ext.ruleSetFile).exists()) {
-                    it.ruleSetFiles = root.files(root.file(ext.ruleSetFile))
-                }
             }
 
             target.tasks.withType(Pmd::class.java) {
+                it.source = target.fileTree(ext.source)
                 it.exclude(ext.exclude)
                 it.include(ext.include)
+                it.reports.html.isEnabled = ext.htmlReportEnabled
+                it.reports.xml.isEnabled = ext.xmlReportEnabled
             }
         }
+    }
+
+    private fun createDefaultRuleSet(file: File) {
+        file.parentFile.mkdirs()
+        File(this.javaClass.getResource("/pmd-default-ruleset.xml").toURI()).copyTo(file, false)
     }
 }
--- src/main/kotlin/com/github/neherim/quality/check/tools/PmdQualityExtension.kt
+++ src/main/kotlin/com/github/neherim/quality/check/tools/PmdQualityExtension.kt
@@ -1,11 +1,14 @@
 package com.github.neherim.quality.check.tools
 
 open class PmdQualityExtension {
-    var enabled: Boolean = true
-    var toolVersion: String = "6.0.0"
-    var ruleSetFile: String = "config/pmd/pmd.xml"
+    var enabled = true
+    var toolVersion = "6.9.0"
+    var ruleSetFile = "config/pmd/pmd.xml"
     var ignoreFailures: Boolean = false
-    var include: List<String> = listOf("**/*.java")
+    var source = "src"
+    var include: List<String> = listOf()
     var exclude: List<String> = listOf()
     var consoleOutput = true
+    var xmlReportEnabled = true
+    var htmlReportEnabled = true
 }
